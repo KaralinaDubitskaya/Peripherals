29d11c19762fa45a00ece141fcfb25bd6e4dd8ac
Karolina Dubitskaya <Karolina.D100899@gmail.com>
2017-12-06 03:02:34 +0300

    Changed address calculation

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 #include "pci.h"
 
 
-int getV(unsigned short int data[2])
-{
+int getV(unsigned short int data[2]) {
     int i = 0;
     for (i; i < PCI_VENTABLE_LEN; i++)
     {
@@ -19,8 +18,7 @@ int getV(unsigned short int data[2])
 }
 
 
-int getD(unsigned short int data[2])
-{
+int getD(unsigned short int data[2]) {
     int i = 0;
     for (i; i < PCI_DEVTABLE_LEN; i++)
     {
@@ -37,21 +35,20 @@ int getD(unsigned short int data[2])
     return 0;
 }
 
-void getDevVen(unsigned int *datar)
-{
+void getDevVen(unsigned int *datar) {
     unsigned short int *data;
-    data = (unsigned short int*)datar;
+    data = (unsigned short int *) datar;
     getV(data);
     getD(data);
 }
 
-void getInterruptInfo(unsigned int data1)
-{
-    char *data = (char*)&data1;
+void getInterruptInfo(unsigned int data1) {
+    char *data = (char *) &data1;
     printf("Interrupt line: ");
     switch (data[0])
     {
-        case 255: printf("255(unknown input or not used)\n");
+        case 255:
+            printf("255(unknown input or not used)\n");
             break;
         default:
             printf("%d\n", data[0]);
@@ -60,17 +57,23 @@ void getInterruptInfo(unsigned int data1)
     printf("Interrupt pin: ");
     switch (data[1])
     {
-        case 0: printf("not used(0)\n");
+        case 0:
+            printf("not used(0)\n");
             break;
-        case 1: printf("INTA#(1)\n");
+        case 1:
+            printf("INTA#(1)\n");
             break;
-        case 2: printf("INTB#(2)\n");
+        case 2:
+            printf("INTB#(2)\n");
             break;
-        case 3: printf("INTC#(3)\n");
+        case 3:
+            printf("INTC#(3)\n");
             break;
-        case 4: printf("INTD#(4)\n");
+        case 4:
+            printf("INTD#(4)\n");
             break;
-        case 5: printf("FFh reserve(5)\n");
+        case 5:
+            printf("FFh reserve(5)\n");
             break;
         default:
             printf("%d\n", data[1]);
@@ -78,78 +81,18 @@ void getInterruptInfo(unsigned int data1)
     }
 }
 
-void getPortInfo(unsigned int *datar)
-{
+void getPortInfo(unsigned int *datar) {
     char *data;
-    data = (char*)datar;
+    data = (char *) datar;
     printf("Primary bus number: %d\n", data[0]);
     printf("Secondary bus number: %d\n", data[1]);
     printf("Subordinate bus number: %d\n", data[2]);
 }
 
-void getInfo()
-{
-    unsigned int addr = 0x80000000;
-    unsigned int bus = addr;
-    char i, j, k;
-    i = 0;
-    for (bus; bus <= 0x80FF0000; bus += 0x10000)
-    {
-        j = 0;
-        unsigned int dev = bus;
-        for (dev; dev <= bus + 0xF800; dev += 0x800)
-        {
-            k = 0;
-            unsigned int func = dev;
-            for (func; func <= dev + 0x700; func += 0x100)
-            {
-                outl(func, 0xCF8);
-                unsigned int data = inl(0xCFC);
-                if (data != 0xFFFFFFFF)
-                {
-                    printf("%d:%d:%d\n", i, j, k);
-                    printf("%x\n", data);
-                    getDevVen(&data);
-                    func += 0x0C;
-                    outl(func, 0xCF8);
-                    data = inl(0xCFC);
-                    if (!(data & 0x10000))
-                    {
-                        printf("not port\n");
-                        func -= 0x0C;
-                        func += 0x3C;
-                        outl(func, 0xCF8);
-                        data = inl(0xCFC);
-                        getInterruptInfo(data);
-                        func -= 0x3C;
-                    }
-                    else
-                    {
-                        printf("port\n");
-                        func -= 0x0C;
-                        func += 0x18;
-                        outl(func, 0xCF8);
-                        data = inl(0xCFC);
-                        getPortInfo(&data);
-                        func -= 0x18;
-                    }
-                    printf("---------------------\n");
-                }
-                else
-                    break;
-                k++;
-            }
-            j++;
-        }
-        i++;
-    }
-}
-
 // Составить адрес конфигурационного регистра по номеру шины,
 // номеру устройства, номеру функции и номеру регистра
-unsigned long calculateAddress(int bus, int device, int function, int reg)
-{
-    unsigned long address = 1;
+unsigned int calculateAddress(int bus, int device, int function, int reg) {
+    unsigned int address = 1;
     address = address << 15;
     address += bus;          // Номер шины, 8 бит
     address = address << 5;
@@ -161,15 +104,57 @@ unsigned long calculateAddress(int bus, int device, int function, int reg)
     return address;
 }
 
-int main(void)
-{
-    printf("\n\nbegin\n");
-    if(iopl(3))
+void ShowDeviceInfo(int bus, int dev, int fun) {
+    //
+    unsigned int configAddress = calculateAddress(bus, dev, fun, 0x00);
+    //
+    outl(configAddress, 0xCF8);
+    unsigned int regData = inl(0xCFC);
+    if (regData != 0xFFFFFFFF)
+    {
+        //printf("%d:%d:%d\n", i, j, k);
+        printf("%x\n", regData);
+        getDevVen(&regData);
+        configAddress += 0x0C;
+        outl(configAddress, 0xCF8);
+        regData = inl(0xCFC);
+        if (!(regData & 0x10000))
+        {
+            printf("not port\n");
+            configAddress -= 0x0C;
+            configAddress += 0x3C;
+            outl(configAddress, 0xCF8);
+            regData = inl(0xCFC);
+            getInterruptInfo(regData);
+            configAddress -= 0x3C;
+        } else
+        {
+            printf("port\n");
+            configAddress -= 0x0C;
+            configAddress += 0x18;
+            outl(configAddress, 0xCF8);
+            regData = inl(0xCFC);
+            getPortInfo(&regData);
+            configAddress -= 0x18;
+        }
+        printf("---------------------\n");
+    }
+
+}
+
+int main(void) {
+    //Granted privileges 3 for port access
+    if (iopl(3))
     {
         printf("I/O Privilege level change error.\nTry running under ROOT user\n");
         return 1;
-    } //granted privileges 3 for port access
-    getInfo();
-    printf("end\n");
+    }
+
+    //main cycle
+    for (int bus = 0; bus < 256; bus++)              // Bus number, 8 bits, 2^8 = 256
+        for (int dev = 0; dev < 32; dev++)           // Device number, 5 bits, 2^5 = 32
+            for (int func = 0; func < 8; func++)     // Function number, 3 bits, 2^3 = 8
+                ShowDeviceInfo(bus, dev, func);
+
     return 0;
 }
